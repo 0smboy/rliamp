f7e1919463e76e2605b6b3e2e4ad0545e78378d0
0smboy <osmboy@buaa.edu.cn>
2026-03-02 14:32:17 +0800

    chore(release): update formula for v0.1.10

--- Formula/rliamp.rb
+++ Formula/rliamp.rb
@@ -1,8 +1,8 @@
 class Rliamp < Formula
   desc "Retro terminal music player with visualizer and EQ"
   homepage "https://github.com/0smboy/rliamp"
-  url "https://github.com/0smboy/rliamp/releases/download/v0.1.9/rliamp-v0.1.9-src.tar.gz"
-  sha256 "201b7dc0bfbad205590142ba9c357d9639a4ff04e8065de748509ff6c136d5dc"
+  url "https://github.com/0smboy/rliamp/releases/download/v0.1.10/rliamp-v0.1.10-src.tar.gz"
+  sha256 "ac30e96c35502c6f252181c68679389cd9e533e90f1acf9fdd73d984835448c2"
   license :cannot_represent
 
   depends_on "rust" => :build
